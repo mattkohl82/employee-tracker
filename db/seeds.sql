fa7cc343260bb57a66eb691c7c3a43da2c54f223
mattkohl82 <mattkohl82@gmail.com>
2020-06-08 14:49:24 -0500

    functions and inquiry

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,27 @@
-INSERT INTO department (department_name)
+USE employees;
+
+INSERT INTO departments (department_name)
 VALUES
 ('Engineering'),
 ('Admin'),
 ('Design'),
 ('Labor');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-('Lead', 55000, 1 ),
+('Manager', 55000, 1),
 ('CSS', 38000, 2),
 ('JS', 44000, 3),
 ('HTML', 48000, 4),
 ('Entry Level', 32000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES
-('Steve', 'Austin', 1, 1),
+('Steve', 'Austin', 1, null),
 ('Drew', 'McIntyre', 2, 1),
 ('John', 'Cena', 2, 1),
 ('Dwayne', 'Johnson', 3, 1),
-('Shawn', 'Micheals', 4, 2),
-('Becky', 'Lynch', 4, 2),
-('Adam', 'Cole', 4, 2),
-('Pete', 'Dune', 4, 2);
+('Shawn', 'Micheals', 4, 1),
+('Becky', 'Lynch', 4, 1),
+('Adam', 'Cole', 4, 1),
+('Pete', 'Dune', 4, 1);
